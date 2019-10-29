5619092cf2f76d8d2adc21431db81fb8d57ba2fb
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-10-29 21:55:55 +0700

    Update profile.dart

--- app/lib/profile.dart
+++ app/lib/profile.dart
@@ -10,25 +10,55 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  FirebaseUser _user;
+
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.currentUser().then((user) {
+      setState(() {
+        _user = user;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
       ),
-      body: Center(
-        child: RaisedButton(
-          color: MERAH,
-          textColor: Colors.white,
-          child: Text('LOGOUT'),
-          onPressed: () async {
-            await GoogleSignIn().signOut();
-            await FirebaseAuth.instance.signOut();
-            Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context) => Splash()));
-          },
-        ),
-      ),
+      body: _user == null
+          ? Center(child: CircularProgressIndicator())
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(48),
+                    child: Image.network(
+                      _user.photoUrl,
+                      width: 96,
+                      height: 96,
+                    ),
+                  ),
+                  Text(_user.displayName, style: TextStyle(fontSize: 20)),
+                  Text(_user.email, style: TextStyle(color: MERAH)),
+                  Text(_user.uid),
+                  RaisedButton(
+                    color: MERAH,
+                    textColor: Colors.white,
+                    child: Text('LOGOUT'),
+                    onPressed: () async {
+                      await GoogleSignIn().signOut();
+                      await FirebaseAuth.instance.signOut();
+                      Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(builder: (context) => Splash()));
+                    },
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }

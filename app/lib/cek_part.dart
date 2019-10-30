a6b824bf2d52c06242d169db34eb4005c7379e11
Stanley Heryanto <stanleyheryanto@Gmail.com>
2019-10-30 15:34:24 +0700

    Update cek_part.dart

--- app/lib/cek_part.dart
+++ app/lib/cek_part.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:toyotaku/main.dart';
 
@@ -12,11 +15,66 @@ class _CekPartState extends State<CekPart> {
 
   void _onQRViewCreated(QRViewController controller) {
     this._controller = controller;
-    controller.scannedDataStream.listen((data) {
+    controller.scannedDataStream.listen((data) async {
       controller.pauseCamera();
-      print(data);
+      Fluttertoast.showToast(msg: 'Checking code...');
+      QuerySnapshot qs = await Firestore.instance
+          .collection('spareparts')
+          .where('kode', isEqualTo: data)
+          .getDocuments();
+      if (qs.documents.isEmpty) {
+        Fluttertoast.showToast(msg: 'Kode tidak valid');
+        controller.resumeCamera();
+        return;
+      }
+      await Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => PartDetail(
+            ds: qs.documents.first,
+          ),
+        ),
+      );
       controller.resumeCamera();
     });
+  }
+
+  void _inputKode() async {
+    _controller.pauseCamera();
+    String kode = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: TextField(
+            decoration: InputDecoration(hintText: 'Kode Unik'),
+            onSubmitted: (s) => Navigator.of(context).pop(s),
+          ),
+        );
+      },
+    );
+
+    if (kode == null) {
+      _controller.resumeCamera();
+      return;
+    }
+
+    Fluttertoast.showToast(msg: 'Checking code...');
+    QuerySnapshot qs = await Firestore.instance
+        .collection('spareparts')
+        .where('kode', isEqualTo: kode)
+        .getDocuments();
+    if (qs.documents.isEmpty) {
+      Fluttertoast.showToast(msg: 'Kode tidak valid');
+      _controller.resumeCamera();
+      return;
+    }
+    await Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => PartDetail(
+          ds: qs.documents.first,
+        ),
+      ),
+    );
+    _controller.resumeCamera();
   }
 
   @override
@@ -54,7 +112,7 @@ class _CekPartState extends State<CekPart> {
                 RaisedButton(
                   textColor: MERAH,
                   child: Text('KODE'),
-                  onPressed: () {},
+                  onPressed: _inputKode,
                 ),
               ],
             ),
@@ -88,6 +146,120 @@ class _CekPartState extends State<CekPart> {
             ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class PartDetail extends StatefulWidget {
+  final DocumentSnapshot ds;
+  const PartDetail({Key key, @required this.ds}) : super(key: key);
+  @override
+  _PartDetailState createState() => _PartDetailState();
+}
+
+class _PartDetailState extends State<PartDetail> {
+  void _pilihMobil() async {
+    FirebaseUser user = await FirebaseAuth.instance.currentUser();
+    bool result = await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: StreamBuilder<QuerySnapshot>(
+              stream: Firestore.instance
+                  .collection('users')
+                  .document(user.uid)
+                  .collection('mobil')
+                  .orderBy('created_at', descending: true)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) return Text(snapshot.error);
+                if (snapshot.connectionState == ConnectionState.waiting)
+                  return Center(child: CircularProgressIndicator());
+                return ListView(
+                  shrinkWrap: true,
+                  children: snapshot.data.documents.map((doc) {
+                    return ListTile(
+                      title: Text(doc['tipe']),
+                      onTap: () {
+                        Firestore.instance
+                            .collection('mobil')
+                            .document(doc.documentID)
+                            .collection('spareparts')
+                            .add(widget.ds.data);
+                        widget.ds.reference.updateData({'new': false});
+                        Navigator.of(context).pop(true);
+                      },
+                    );
+                  }).toList(),
+                );
+              },
+            ),
+          );
+        });
+    if (result ?? false) {
+      Navigator.of(context).pop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Part Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'ORIGINAL',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: MERAH,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text('Nama'),
+            Text(widget.ds['nama'], style: TextStyle(fontSize: 18)),
+            SizedBox(height: 8),
+            Text('Parts Number'),
+            Text(widget.ds['parts_number'], style: TextStyle(fontSize: 18)),
+            SizedBox(height: 8),
+            Text('Tipe Mobil'),
+            Text(widget.ds['mobil'], style: TextStyle(fontSize: 18)),
+            SizedBox(height: 8),
+            Text('Kode'),
+            Text(widget.ds['kode'], style: TextStyle(fontSize: 18)),
+            SizedBox(height: 8),
+            Text('Kondisi'),
+            Text(
+              widget.ds['new'] ? 'BRAND NEW' : 'SECOND HAND',
+              style: TextStyle(
+                fontSize: 18,
+                color: MERAH,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'REGISTERED',
+              style: TextStyle(
+                fontSize: 18,
+                color: MERAH,
+              ),
+            ),
+            SizedBox(height: 8),
+            RaisedButton(
+              color: MERAH,
+              textColor: Colors.white,
+              child: Text('ADD TO MY CAR'),
+              onPressed: _pilihMobil,
+            ),
+          ],
+        ),
       ),
     );
   }
